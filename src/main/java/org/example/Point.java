305c3eb06d5c9cb9b12158df4330821fef109755
Müge Şahin <mugesahin0@gmail.com>
2024-06-27 01:59:48 +0300

    .

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class Point {
+    private int x;
+    private int y;
+
+
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public double distance(){
+        return Math.sqrt(x*x + y*y);
+    }
+    public double distance(int x, int y){
+        return Math.sqrt((this.x - x)*(this.x - x) + (this.y - y)*(this.y - y));
+    }
+    public double distance(Point another){
+        return Math.sqrt((this.x - another.x)*(this.x - another.x) + (this.y - another.y)*(this.y - another.y));
+    }
+
+    public static void main(String[] args) {
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+    }
+
+}
